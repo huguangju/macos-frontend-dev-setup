4781ac40e5a35a007587c278e566b5aeeea0effa
jinzhanglin <jinzhanglin@hotmail.com>
2025-03-27 23:08:04 +0800

    feat: ignore target

---
+++ macdevkit-cli/target/release/build/macdevkit-cli-203cabb1bfeb09e8/build_script_build-203cabb1bfeb09e8.d
@@ -0,0 +1,5 @@
+/Users/linjinzhang/Workspace/MacDevKit/macdevkit-cli/target/release/build/macdevkit-cli-203cabb1bfeb09e8/build_script_build-203cabb1bfeb09e8: build.rs
+
+/Users/linjinzhang/Workspace/MacDevKit/macdevkit-cli/target/release/build/macdevkit-cli-203cabb1bfeb09e8/build_script_build-203cabb1bfeb09e8.d: build.rs
+
+build.rs:
